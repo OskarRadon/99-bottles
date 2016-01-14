a7bbbcfdb853abf5cd397d72c7d5b0b2bc2b44f3
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-13 16:15:50 -0800

    add style

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(function() {
 
   for (var index = 99; index >= 1; index -= 1) {
     $("#content").append("<p>" + index + " bottles of beer on the wall " + index + " bottles of beer. Take one down pass it around " + (index-1) + " bottles of beer on the wall.</p>" );
+    $(index).addClass("bottles");
+
   };
+
 
 });
