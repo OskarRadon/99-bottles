d938179cdf648650934df4a4036c289a50e719e7
Afton Downey and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-13 16:00:45 -0800

    add div and p tags to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(function() {
+
   for (var index = 99; index >= 1; index -= 1) {
-    $("p").append(" " + index + " bottles of beer on the wall " + index + " bottles of beer. Take one down pass it around " + (index-1) + " bottles of beer on the wall.");
+    $("#content").append("<p>" + index + " bottles of beer on the wall " + index + " bottles of beer. Take one down pass it around " + (index-1) + " bottles of beer on the wall.</p>" );
   };
 
 });
